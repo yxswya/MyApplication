f30fc67ca627c27e26206e961d1d6c04a081bf3f
czq040902 <16615812538@163.com>
2026-02-27 07:46:32 +0800

    first commit

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,21 @@
 import { treaty } from "@elysiajs/eden";
 import type app from "../src";
+import { createIcons, Menu, ArrowRight, Anchor, Download, Clapperboard, Rss, NotebookText } from 'lucide';
+import './global.css'
+
+createIcons({
+    attrs: {
+        class: "lucide-icon",
+    },
+    icons: {
+        Menu,
+        ArrowRight,
+        Anchor,
+        Download,
+        Clapperboard,
+        NotebookText,
+    }
+});
 
 const api = treaty<typeof app>("localhost:3000");
 
